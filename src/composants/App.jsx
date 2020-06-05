718452a69009435ca197fd8f5ad2d2e4dd366909
Christian Lisangola <christian.lisangola@gmail.com>
2020-06-05 10:29:54 +0100

    Affichage des items dans un composant des liste

--- src/composants/App.jsx
+++ src/composants/App.jsx
@@ -1,41 +1,36 @@
 import React from 'react';
-import Card from './Card';
-import Card2 from './Card2';
+import ListeDesCartes from './ListeDesCartes';
 
 //JSX
-const profile = {
-  photo:
-    'https://qph.fs.quoracdn.net/main-thumb-221356592-200-zyhrpkhlegkmkbjildezexzodkngkjec.jpeg',
-  noms: 'Christian Lisangola',
-  anneeInscription: 2019,
-  bio: 'Je suis amoureux des zombies',
-  nombreAmis: 566,
-};
-
-const profile2 = {
-  photo:
-    'https://pbs.twimg.com/profile_images/1097778598294028288/JfH7BFgX_400x400.jpg',
-  noms: 'Laurene Bitota',
-  anneeInscription: 1999,
-  bio: 'Je suis développeuse',
-  nombreAmis: 899,
-};
+const profiles = [
+  {
+    photo:
+      'https://qph.fs.quoracdn.net/main-thumb-221356592-200-zyhrpkhlegkmkbjildezexzodkngkjec.jpeg',
+    noms: 'Christian Lisangola',
+    anneeInscription: 2019,
+    bio: 'Je suis amoureux des zombies',
+    nombreAmis: 566,
+  },
+  {
+    photo:
+      'https://pbs.twimg.com/profile_images/1097778598294028288/JfH7BFgX_400x400.jpg',
+    noms: 'Laurene Bitota',
+    anneeInscription: 1999,
+    bio: 'Je suis développeuse',
+    nombreAmis: 899,
+  },
+  {
+    photo:
+      'https://pbs.twimg.com/profile_images/1040675969634324481/C3Rw5Pqq.jpg',
+    noms: 'Dieudonné Miyalu',
+    anneeInscription: 2015,
+    bio: 'Lead developpeur',
+    nombreAmis: 700,
+  },
+];
 
 function App() {
-  return (
-    <>
-      <Card
-        photo={profile.photo}
-        noms={profile.noms}
-        anneeInscription={profile.anneeInscription}
-        bio={profile.bio}
-        nombreAmis={profile.nombreAmis}
-      />
-
-      <Card {...profile2} />
-      <Card2 {...profile} />
-    </>
-  );
+  return <ListeDesCartes profiles={profiles} />;
 }
 
 export default App;
